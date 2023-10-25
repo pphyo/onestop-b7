400ef20c5f8842a7954acd5a90f7e2f8ef1803e4
pphyo <pyaephyo.jdc@gmail.com>
2023-10-25 11:55:55 +0630

    Exception and Vararg

---
+++ 01-Java-SE/api01-exception/src/test/java/com/jdc/exept/test/ExceptionTest.java
@@ -0,0 +1,28 @@
+package com.jdc.exept.test;
+
+import java.util.Properties;
+
+import org.junit.jupiter.api.Test;
+
+public class ExceptionTest {
+	
+	@Test
+	void test_property() {
+		Properties props = System.getProperties();
+		props.list(System.out);
+		
+		System.out.println(System.getProperty("user.home"));
+	}
+	
+//	@Test
+	void test() {
+		System.out.println("Before exception...");
+		check(null);
+		System.out.println("After exception...");
+	}
+	
+	void check(String data) {
+		System.out.println(data.charAt(0));
+	}
+
+}
